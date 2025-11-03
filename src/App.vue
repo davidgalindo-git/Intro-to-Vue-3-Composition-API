10091acb5f3b449047fd4dfb28f2da5e5c554c45
David Galindo Salamanca <pq87nkq@eduvaud.ch>
2025-11-03 15:47:09 +0100

    feat:end of ex.7

--- src/App.vue
+++ src/App.vue
@@ -4,32 +4,39 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = false
-  
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
-
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage},
+  { id: 0, color: 'green', image: socksGreenImage, quantity: 10, onSale: true},
+  { id: 1, color: 'blue', image: socksBlueImage, quantity: 0, onSale: false},
 ])
-
 const cart = ref(0)
+const brand = ref('CPNV')
+const selectedVariant = ref(0)
 
+function sale(){
+  if (variants.value[selectedVariant.value].onSale) {
+  return " est en action !"  }
+}
+function image() {
+  return variants.value[selectedVariant.value].image
+}
+function inStock() {
+  return variants.value[selectedVariant.value].quantity > 0
+}
 function addToCart() {
   cart.value++
 }
-
 function subtractFromCart() {
   if (cart.value > 0) {
     cart.value--
   }
 }
-
-function changeImage(itemPath) {
- image.value = itemPath
+function brandedProduct() {
+  return brand.value + ' ' + product.value
 }
-
+function updateVariant(variantId) {
+  selectedVariant.value = variantId
+}
 </script>
   
 <template>
@@ -38,26 +45,26 @@ function changeImage(itemPath) {
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">    
-        <img v-if="inStock" v-bind:src="image" alt="Socks"/>
-        <img v-else="inStock" v-bind:src="image" alt="Socks" class="out-of-stock-img"/>
+        <img v-if="inStock()" v-bind:src="image()" alt="Socks"/>
+        <img v-else v-bind:src="image()" alt="Socks" class="out-of-stock-img"/>
 
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
+        <h1>{{ brandedProduct() }}</h1>
+        <p>{{ sale() }}</p>
+        <p v-if="inStock()">In Stock</p>
         <p v-else>Out of Stock</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div class="color-circle"
-          @mouseover="changeImage(variant.image)"
           v-for="variant in variants"
           :key="variant.id"
           :style="{ backgroundColor: variant.color }"
-          
+          @mouseover="updateVariant(variant.id)"
         >
         </div>
-        <button v-if="inStock==true" @click="addToCart()" >Ajouter</button>
+        <button v-if="inStock()" @click="addToCart()" >Ajouter</button>
         <button v-else class="disabledButton">Ajouter</button>
         <button @click="subtractFromCart()">Enlever du panier</button>
       </div>
