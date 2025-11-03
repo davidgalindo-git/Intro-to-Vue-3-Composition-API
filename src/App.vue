b3e1294707adebf1b32accc50ebbb9e9beaeb419
David Galindo Salamanca <pq87nkq@eduvaud.ch>
2025-11-03 14:31:36 +0100

    feat:end of ex.5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
@@ -9,14 +10,24 @@ const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreenImage },
+  { id: 2235, color: 'blue', image: socksBlueImage},
 ])
 
 const cart = ref(0)
 
 function addToCart() {
   cart.value++
+}
+
+function subtractFromCart() {
+  if (cart.value > 0) {
+    cart.value--
+  }
+}
+
+function changeImage(itemPath) {
+ image.value = itemPath
 }
 
 </script>
@@ -27,7 +38,7 @@ function addToCart() {
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">    
-        <img v-bind:src="image">
+        <img v-bind:src="image" alt="Socks"/>
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
@@ -39,10 +50,12 @@ function addToCart() {
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="changeImage(variant.image)"
         >
           {{ variant.color }}
         </div>
-        <button v-on:click="addToCart()">Add to Cart</button>
+        <button @click="addToCart()">Add to Cart</button>
+        <button @click="subtractFromCart()">Subtract from Cart</button>
       </div>
     </div>
   </div>
